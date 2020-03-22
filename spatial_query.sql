a464d6a7825bc4469ddb9cb681095c597addeee9
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-03-22 15:25:02 +0100

    update testfiles and spatial request

--- spatial_query.sql
+++ spatial_query.sql
@@ -1,10 +1,12 @@
---Eingangsparameter: Punkt der Suche, maximaler Abstand, quality_ids als suchfilter
+--Eingangsparameter: id der request, Abstand der Suche (Aktuell müssen die qualities noch manuell abgefragt werden. Es existiert aber auch ein Testdatensatz request_quality, aus dem eine Liste der benötigten qualities gezogen werden kann. Das muss noch gemacht werden.
 --Ausgabe: Alle User , die sich innerhalb des maximalen Abstands vom Suchpunkt befinden und gewünschte Eigenschaften mitbringen. Der Standort wird als GeoJSON codiert ausgegeben ==> {"type":"Point","crs":{"type":"name","properties":{"name":"EPSG:4326"}},"coordinates":[9.910927,53.32021]}
 
-WITH close_users as (
+WITH close_users AS (
 	SELECT users.*
-	FROM users, poc
-	WHERE ST_DWithin(users.geom::geography, poc.geom::geography, 5000)
+	FROM users
+	WHERE ST_DWithin(users.geom::geography, 
+		(SELECT request.geom FROM request
+		WHERE id=102)::geography, 10000)
 	)
 SELECT ST_AsGeoJson(close_users.geom,6,2), close_users.first_name, close_users.last_name, close_users.email, close_users.id
 FROM   close_users
